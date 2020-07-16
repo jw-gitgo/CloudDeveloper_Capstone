3800f31259d4b9a23c65d1ad50c1afeced097181
jw-gitgo <jmrwnd@yahoo.com>
2020-07-16 16:07:47 -0400

    trying to get route API calls working

--- backend/src/lambda/http/createTrip.ts
+++ backend/src/lambda/http/createTrip.ts
@@ -4,9 +4,14 @@ import { CreateTripRequest } from '../../requests/CreateTripRequest';
 import * as uuid from 'uuid';
 import { decode } from 'jsonwebtoken';
 import { JwtPayload } from '../../auth/JwtPayload';
+import Axios from 'axios';
 import * as AWS from 'aws-sdk'
 import { createLogger } from '../../utils/logger';
 const logger = createLogger('createTrip');
+
+const routeAPIKey = process.env.ROUTE_API_KEY
+const routeAPIUrl = 'https://api.openrouteservice.org/geocode/search?api_key=';
+const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
 
 const docClient = new AWS.DynamoDB.DocumentClient()
 const tripsTable = process.env.TRIPS_TABLE
@@ -47,6 +52,10 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
   const decodedJwt = decode(jwtToken) as JwtPayload
   const userId = decodedJwt.sub;
   const createdAt = new Date(Date.now()).toISOString();
+
+  const cleanedStart1 = newTrip.startPoint.replace(regex, '');
+  const cleanedStart2 = cleanedStart1.replace(' ', '%20');
+  const startGeo = await Axios.get(routeAPIUrl+routeAPIKey+'&text='+cleanedStart2);
   
 
   const tripItem = {
@@ -54,7 +63,8 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
     tripId,
     createdAt,
     tripIconUrl: `https://${process.env.S3_BUCKET}.s3.amazonaws.com/${tripId}`,
-    ...newTrip
+    ...newTrip,
+    startGeo
   };
 
   await docClient.put({
--- backend/src/lambda/http/getTrips.ts
+++ backend/src/lambda/http/getTrips.ts
@@ -1,7 +1,7 @@
 import 'source-map-support/register';
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda';
 import { decode } from 'jsonwebtoken';
-//import Axios from 'axios';
+import Axios from 'axios';
 import { JwtPayload } from '../../auth/JwtPayload';
 import * as AWS from 'aws-sdk'
 import { createLogger } from '../../utils/logger';
@@ -10,7 +10,9 @@ const logger = createLogger('getTrips');
 const docClient = new AWS.DynamoDB.DocumentClient()
 const tripsTable = process.env.TRIPS_TABLE
 const indexName = process.env.INDEX_NAME
-//const routeAPIKey = process.env.ROUTE_API_KEY
+const routeAPIKey = process.env.ROUTE_API_KEY
+const routeAPIUrl = 'https://api.openrouteservice.org/geocode/search?api_key=';
+const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   
@@ -34,11 +36,13 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
 
 ///////////////////////////////////////////////
 
-  items.forEach(function(item) {
-    item.startGeo = "dummy test value";
-});
+  items.forEach(async function(item) {
+    var cleanedStart1 = item.startPoint.replace(regex, '');
+    var cleanedStart2 = cleanedStart1.replace(' ', '%20');
+    item.startGeo = await getCoordinates(cleanedStart2);
+  });
 
-/*
+
   Axios.get('https://api.openrouteservice.org/geocode/search?api_key='+routeAPIKey+'&text=Namibian%20Brewery')
   .then(response => {
     console.log(response.data.url);
@@ -47,7 +51,7 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
   .catch(error => {
     console.log(error);
   });
-  */
+  
 
   return {
     statusCode: 200,
@@ -59,4 +63,9 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
       items: items
     })
   };
+}
+
+async function getCoordinates (cleanedStart2: string): Promise<string> {
+  const location = await Axios.get(routeAPIUrl+routeAPIKey+'&text='+cleanedStart2);
+  return location.data.features.geometry.coordinates;
 }
