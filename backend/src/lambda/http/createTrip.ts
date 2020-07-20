d2c9e250e552e1ad86b54769d02f07be77ddb2e2
jw-gitgo <jmrwnd@yahoo.com>
2020-07-20 16:45:34 -0400

    fixed JSON drilldown error

--- backend/src/lambda/http/createTrip.ts
+++ backend/src/lambda/http/createTrip.ts
@@ -58,9 +58,9 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
 
   const wayPointCount = route.properties.way_points[1];
   console.log("wayPointCount: ",wayPointCount);
-  const quarterGeo = route.features[0].geometry.coordinates[0][Math.round(wayPointCount/4)];
-  const halfGeo = route.features[0].geometry.coordinates[0][Math.round(wayPointCount/2)];
-  const threequarterGeo = route.features[0].geometry.coordinates[0][Math.round(wayPointCount*3/4)];
+  const quarterGeo = route.geometry.coordinates[0][Math.round(wayPointCount/4)];
+  const halfGeo = route.geometry.coordinates[0][Math.round(wayPointCount/2)];
+  const threequarterGeo = route.geometry.coordinates[0][Math.round(wayPointCount*3/4)];
 
   const weather = await getWeather(startGeo, quarterGeo, halfGeo, threequarterGeo, endGeo, duration);
   console.log("weather: ", weather);
